af3606affcb4d01a88c027c432e7def52d3174f1
kanakdayma <79441689+kanakdayma@users.noreply.github.com>
2024-11-25 21:47:10 +0530

    main

---
+++ script.js
@@ -0,0 +1,5 @@
+<script>
+document.getElementById("submit-btn").addEventListener("click", function() {
+    // If you want to simulate a successful form submission
+    alert("Your information has been sent!");
+</script>
